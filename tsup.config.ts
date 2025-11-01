53e11d4741d19ba05e901ded9082a704cb5c4d37
Orestes Perez <orestespp96@gmail.com>
2025-11-01 22:58:44 +0100

    chore: adjust tsup config for Solid JSX build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
     dts: true,
     clean: true,
     esbuildOptions(options) {
-        // Usa el transformador JSX de Solid
-        options.jsx = 'transform';
-        options.jsxFactory = 'h';
-        options.jsxFragment = 'Fragment';
-        options.jsxImportSource = 'solid-js';
+        options.jsx = "automatic";
+        options.jsxImportSource = "solid-js";
     },
 });
